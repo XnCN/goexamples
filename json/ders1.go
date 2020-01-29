8b45420fff2c99f6f7e5a330349f7e57f1029e2a
Eray Han <eray@Eray-MacBook-Pro.local>
2020-01-30 00:28:09 +0300

    json işlemleri

---
+++ json/ders1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Customer struct {
+	FirstName string
+	LastName string
+	Age int
+}
+func main() {
+	jsonString:=`{"firstname":"Aydemir Eray","lastname":"Han","age":22}`
+	var myCustomer Customer
+	json.Unmarshal([]byte(jsonString),&myCustomer)
+	fmt.Printf("Customer Name:%v,Surname:%v,Age:%v \n",myCustomer.FirstName,myCustomer.LastName,myCustomer.Age)
+
+	//objeden json'a
+	j,_:=json.Marshal(myCustomer)
+	fmt.Println(string(j))
+}
